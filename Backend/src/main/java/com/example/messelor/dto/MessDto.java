aeaa96d64cecdfd73283677aa91371e5fca31eb2
Adnan Borshon <adnan.borshon@gmail.com>
2025-09-18 21:25:49 +0600

    Delete Backend directory

--- Backend/src/main/java/com/example/messelor/dto/MessDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.messelor.dto;
-
-import lombok.*;
-@Data@NoArgsConstructor@AllArgsConstructor
-public class MessDto{private String messName;private String messCode;private String description;private String address;private Double monthlyServiceCharge;private Integer maxMembers;private Integer managerId;private Boolean isActive;}
--- Backend/src/main/java/com/example/messelor/dto/UserDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.messelor.dto;
-
-import lombok.*;
-@Data@NoArgsConstructor@AllArgsConstructor
-public class UserDto{private String username;private String email;private String password;}
-
