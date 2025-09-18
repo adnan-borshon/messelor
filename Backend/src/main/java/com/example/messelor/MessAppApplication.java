cb67dbcca3ad86deacb86860c7cf0bdd6a02e008
adnan-borshon <adnan.borshon@gmail.com>
2025-09-18 21:24:46 +0600

    Backend delete

--- Backend/src/main/java/com/example/messelor/MessAppApplication.java
+++
@@ -1,6 +0,0 @@
-package com.example.messelor;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class MessAppApplication{public static void main(String[]a){SpringApplication.run(MessAppApplication.class,a);}}
-
