8953c015b933b76a65ed308db041743b70656b58
A. F. M Shahriar Sharif <46073112+afmssrahi@users.noreply.github.com>
2025-09-19 11:54:18 +0600

    Delete backend directory

--- backend/src/main/java/com/example/backend/payload/JwtResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.payload;
-
-import java.util.List;
-
-public record JwtResponse(String token, String username, List<String> roles) {}
--- backend/src/main/java/com/example/backend/payload/LoginRequest.java
+++
@@ -1,3 +0,0 @@
-package com.example.backend.payload;
-
-public record LoginRequest(String usernameOrEmail, String password) {}
